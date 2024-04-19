6b8627052874407d8fca22c14b037a90f5b97c2a
Akif Albayrak <akifalbayrak6161@gmail.com>
2024-04-19 14:32:27 +0300

    fix(Header): restyling Caret icon

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom"; // Import Link component from react-router-dom
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDropletSlash } from "@fortawesome/free-solid-svg-icons";
+import {
+    faDropletSlash,
+    faCaretDown,
+    faCaretUp,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(true);
+
     return (
         <>
             <header className="bg-white text-gray-800 flex justify-center">
@@ -17,53 +23,65 @@ const Header = () => {
                             İzmir Büyükşehir Belediyesi Bilgilendirme Sayfası
                         </h1>
                     </Link>
-                    <div className="grid grid-cols-4 gap-4 my-8">
-                        <Link
-                            className="text-blue-500 hover:text-blue-700"
-                            to="/">
-                            Arıza Kaynaklı Düzensiz Su Kesintileri
-                        </Link>
-                        <Link
-                            className="text-blue-500 hover:text-blue-700"
-                            to="/dam-fill-rate">
-                            Baraj Doluluk Oranı
-                        </Link>
-                        <Link
-                            className="text-blue-500 hover:text-blue-700"
-                            to="/daily-water">
-                            Günlük Su Üretimi
-                        </Link>
-                        <Link
-                            className="text-blue-500 hover:text-blue-700"
-                            to="/distribution-of-water-production">
-                            Su Üretiminin Aylara ve Kaynaklara Göre Dağılımı
-                        </Link>
-                        <Link
-                            className="text-blue-500 hover:text-blue-700"
-                            to="/dam-list">
-                            Baraj Listesi
-                        </Link>
-                        <Link
-                            className="text-blue-500 hover:text-blue-700"
-                            to="/dam-water-quality-reports">
-                            Baraj Su Kalite Raporları
-                        </Link>
-                        <Link
-                            className="text-blue-500 hover:text-blue-700"
-                            to="/weekly-water-analysis">
-                            Haftalık Su Analiz Sonuçları
-                        </Link>
-                        <Link
-                            className="text-blue-500 hover:text-blue-700"
-                            to="/historical-water-structures">
-                            Tarihi Su Yapıları Konum Verisi
-                        </Link>
-                        <Link
-                            className="text-blue-500 hover:text-blue-700"
-                            to="/blue-flag-beaches">
-                            Mavi Bayraklı Plajlar Konum Verisi
-                        </Link>
-                    </div>
+                    <button
+                        className="border-2 border-gray-200 rounded-xl px-3 my-2 float-right"
+                        onClick={() => setIsOpen(!isOpen)}>
+                        {isOpen ? (
+                            <FontAwesomeIcon icon={faCaretDown} />
+                        ) : (
+                            <FontAwesomeIcon icon={faCaretUp} />
+                        )}
+                    </button>
+                    {isOpen && (
+                        <div className="grid grid-cols-4 gap-4 my-8 ">
+                            <Link
+                                className="text-blue-500 hover:text-blue-700"
+                                to="/">
+                                Arıza Kaynaklı Düzensiz Su Kesintileri
+                            </Link>
+
+                            <Link
+                                className="text-blue-500 hover:text-blue-700"
+                                to="/daily-water">
+                                Günlük Su Üretimi
+                            </Link>
+                            <Link
+                                className="text-blue-500 hover:text-blue-700"
+                                to="/weekly-water-analysis">
+                                Haftalık Su Analiz Sonuçları
+                            </Link>
+                            <Link
+                                className="text-blue-500 hover:text-blue-700"
+                                to="/distribution-of-water-production">
+                                Su Üretiminin Aylara ve Kaynaklara Göre Dağılımı
+                            </Link>
+                            <Link
+                                className="text-blue-500 hover:text-blue-700"
+                                to="/dam-fill-rate">
+                                Baraj Doluluk Oranı
+                            </Link>
+                            <Link
+                                className="text-blue-500 hover:text-blue-700"
+                                to="/dam-list">
+                                Baraj Listesi
+                            </Link>
+                            <Link
+                                className="text-blue-500 hover:text-blue-700"
+                                to="/dam-water-quality-reports">
+                                Baraj Su Kalite Raporları
+                            </Link>
+                            <Link
+                                className="text-blue-500 hover:text-blue-700"
+                                to="/historical-water-structures">
+                                Tarihi Su Yapıları Konum Verisi
+                            </Link>
+                            <Link
+                                className="text-blue-500 hover:text-blue-700"
+                                to="/blue-flag-beaches">
+                                Mavi Bayraklı Plajlar Konum Verisi
+                            </Link>
+                        </div>
+                    )}
                 </div>
             </header>
             <hr className="border-b border-gray-200" />
