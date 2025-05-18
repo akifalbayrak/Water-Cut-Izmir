cef653a33fee1141963b362a044010caf6895b78
akif albayrak <akifalbayrak6161@gmail.com>
2025-05-18 19:48:10 +0300

    refactor: Streamline DepartmentCashDesk component with consolidated data fetching and improved search functionality

--- src/Components/DepartmentCashDesk.jsx
+++ src/Components/DepartmentCashDesk.jsx
@@ -1,141 +1,98 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { IoIosSearch } from "react-icons/io";
 
 const DepartmentCashDesk = () => {
     const [data, setData] = useState([]);
-    const [data1, setData1] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
-    const [searchTerm1, setSearchTerm1] = useState("");
-    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                setLoading(true);
-                const response = await fetch(
+                const responseSube = await fetch(
                     "https://openapi.izmir.bel.tr/api/izsu/subeler"
                 );
-                const response1 = await fetch(
+                const responseVezne = await fetch(
                     "https://openapi.izmir.bel.tr/api/izsu/vezneler"
                 );
-                const jsonData = await response.json();
-                const jsonData1 = await response1.json();
-                setData(jsonData);
-                setData1(jsonData1);
-                setLoading(false);
+                const jsonDataSube = await responseSube.json();
+                const jsonDataVezne = await responseVezne.json();
+                setData(jsonDataSube.concat(jsonDataVezne));
             } catch (error) {
                 console.error("Error fetching data:", error);
             }
         };
 
         fetchData();
-        // Set up interval to fetch data every 10 minutes
-        const interval = setInterval(fetchData, 600000); // 600000 milliseconds = 10 minute
-
-        // Clean up interval on component unmount
-        return () => clearInterval(interval);
     }, []);
 
-    const changeInput = (e) => {
-        setSearchTerm(e.target.value.toLowerCase());
-        setSearchTerm1(e.target.value.toLowerCase());
-    };
-
+    // Filter data based on search term
     const filteredData = data.filter(
         (item) =>
-            item.SubeAdi?.toLowerCase().includes(searchTerm) ||
-            item.SubeAdresi?.toLowerCase().includes(searchTerm) ||
-            item.SubeTelefon?.includes(searchTerm)
-    );
-
-    const filteredData1 = data1.filter(
-        (item) =>
-            item.VezneAdi?.toLowerCase().includes(searchTerm1) ||
-            item.VezneAdresi?.toLowerCase().includes(searchTerm1) ||
-            item.Bolge?.toLowerCase().includes(searchTerm1)
+            item.VezneAdi?.toLocaleLowerCase("tr-TR").includes(
+                searchTerm.toLowerCase()
+            ) ||
+            item.VezneAdresi?.toLocaleLowerCase("tr-TR").includes(
+                searchTerm.toLowerCase()
+            ) ||
+            item.Bolge?.toLocaleLowerCase("tr-TR").includes(
+                searchTerm.toLowerCase()
+            ) ||
+            item.SubeAdi?.toLocaleLowerCase("tr-TR").includes(
+                searchTerm.toLowerCase()
+            ) ||
+            item.SubeAdresi?.toLocaleLowerCase("tr-TR").includes(
+                searchTerm.toLowerCase()
+            )
     );
 
     return (
-        <main className="DepartmentCashDesk my-4 px-8 py-4 lg:w-3/4 mx-auto ">
-            <div className="container mx-auto flex flex-col justify-center items-center">
-                <div className="items-center mb-2">
-                    <h1 className="text-3xl font-bold text-center text-gray-800">
-                        İZSU Şube ve Vezne Bilgileri
-                    </h1>
-                </div>
-            </div>
-
-            <div className="relative mx-auto max-w-md my-2">
-                <input
-                    type="text"
-                    placeholder="Şube & Vezne Ara"
-                    onChange={changeInput}
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                />
-                <span className="absolute inset-y-0 right-0 flex items-center pr-3">
-                    <IoIosSearch className="text-gray-500" />
-                </span>
-            </div>
-            <div className="card-container my-4 p-4 bg-white rounded-lg shadow-md">
-                {loading && (
-                    <p className="inline-block text-gray-500">Loading...</p>
-                )}
-                {filteredData && (
-                    <>
-                        <p className="font-semibold text-gray-800 my-4 text-3xl">
-                            Şube Bilgileri
-                        </p>
-                        <hr className="border border-gray-400 my-2" />
-                    </>
-                )}
-                {!loading && filteredData.length === 0 && (
-                    <p className="inline-block text-gray-500">
-                        No results found
-                    </p>
-                )}
-
-                {!loading &&
-                    filteredData.map((item, index) => (
-                        <div
-                            key={index}
-                            className={`text-sm my-2 ${
-                                item.AktifMi ? "text-black" : "text-red-800"
-                            }`}>
-                            <p className="font-semibold">{item.SubeAdi}</p>
-                            <p className="text-xs">{item.SubeAdresi}</p>
-                            <p>{item.SubeTelefon}</p>
-                            <hr />
-                        </div>
-                    ))}
-            </div>
-            <div className="card-container my-4 p-4 bg-white rounded-lg shadow-md">
-                {filteredData1 && (
-                    <>
-                        <p className="font-semibold text-gray-800 my-4 text-3xl">
-                            Vezne Bilgileri
-                        </p>
-                        <hr className="border border-gray-400 my-2" />
-                    </>
-                )}
-                {!loading && filteredData1.length === 0 && (
-                    <p className="inline-block text-gray-500">
-                        No results found
-                    </p>
-                )}
-                {!loading &&
-                    filteredData1.map((item, index) => (
-                        <div
-                            key={index}
-                            className={`text-sm my-2 rounded-lg ${
-                                item.AktifMi ? "text-black" : "text-red-800"
-                            }`}>
-                            <p className="font-semibold">{item.VezneAdi}</p>
-                            <p className="text-xs">{item.VezneAdresi}</p>
-                            <p>{item.Bolge}</p>
-                            <hr />
-                        </div>
-                    ))}
-            </div>
+        <main className="p-8 w-[90%] sm:w-[80%] md:w-[70%] lg:w-[60%] mx-auto gap-4 flex flex-col">
+            <section className="flex flex-col justify-center items-center gap-4">
+                <h1 className="text-3xl font-bold text-center">
+                    İZSU Şube ve Vezne Bilgileri
+                </h1>
+                <article className="flex items-center px-3 bg-white py-2 rounded-3xl border border-gray-300 text-2xl w-full md:w-[80%] lg:w-[50%]">
+                    <IoIosSearch className="mr-2" />
+                    <input
+                        type="text"
+                        placeholder="Mahalle veya ilçe ara"
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        className="text-lg bg-transparent border-none rounded w-full focus:outline-none focus:shadow-outline"
+                    />
+                </article>
+            </section>
+            <section className="flex flex-col gap-4">
+                {filteredData.map((item, index) => (
+                    <section
+                        onClick={() => {
+                            window.open(
+                                `https://www.google.com/maps?q=${item.ENLEM},${item.BOYLAM}`,
+                                "_blank"
+                            );
+                        }}
+                        key={index}
+                        className="p-4 border bg-white border-gray-300 rounded-2xl cursor-pointer hover:border-gray-400">
+                        <article className="my-4 flex flex-col md:flex-row items-center gap-4">
+                            <h2 className="text-xl font-semibold my-2">
+                                {item.SubeAdi || item.VezneAdi}
+                            </h2>
+                            <p className="border w-fit p-2 rounded-md text-center">
+                                {item.SubeAdi ? `Şube` : `Vezne`}
+                            </p>
+                            <p className="border w-fit p-2 rounded-md text-center">
+                                {item.VezneAdresi || item.SubeAdresi}
+                            </p>
+                            <p
+                                className={`mx-auto md:mr-0 px-3 py-1 rounded-md text-white text-sm ${
+                                    item.AktifMi ? "bg-green-500" : "bg-red-500"
+                                }`}>
+                                {item.AktifMi ? "Aktif" : "Pasif"}
+                            </p>
+                        </article>
+                    </section>
+                ))}
+            </section>
         </main>
     );
 };
