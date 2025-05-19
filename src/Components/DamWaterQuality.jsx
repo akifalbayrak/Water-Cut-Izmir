4208cee0a6e65fc4a9982d2f4579d26ea4d682a9
akif albayrak <akifalbayrak6161@gmail.com>
2025-05-19 04:02:14 +0300

    refactor: Simplify DamWaterQuality and Modal components with improved structure and search functionality

--- src/Components/DamWaterQuality.jsx
+++ src/Components/DamWaterQuality.jsx
@@ -1,24 +1,19 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { IoIosSearch } from "react-icons/io";
 import Modal from "./Modal";
 
 const DamWaterQuality = () => {
     const [data, setData] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
-    const [limitor, setLimitor] = useState(12);
-    const [loading, setLoading] = useState(false);
     const [selectedItem, setSelectedItem] = useState(null);
-    const [modalIsOpen, setModalIsOpen] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                setLoading(true);
                 const response = await fetch(
                     "https://openapi.izmir.bel.tr/api/izsu/barajsukaliteraporlari"
                 );
                 const jsonData = await response.json();
-                setLoading(false);
                 setData(jsonData.BarajAnalizleri);
             } catch (error) {
                 console.error("Error fetching data:", error);
@@ -26,175 +21,135 @@ const DamWaterQuality = () => {
         };
 
         fetchData();
-        const interval = setInterval(fetchData, 600000); // 600000 milliseconds = 10 minute
-        return () => clearInterval(interval);
     }, []);
-
-    const handleItemClick = (item) => {
-        setSelectedItem(item);
-        setModalIsOpen(true);
-    };
-
-    const closeModal = () => {
-        setModalIsOpen(false);
-    };
 
     const formatDateTime = (dateTimeString) => {
         const dateTime = new Date(dateTimeString);
         return dateTime.toLocaleDateString("tr-TR");
     };
 
-    const changeInput = (e) => {
-        setSearchTerm(e.target.value.toLowerCase());
-    };
-
-    useEffect(() => {
-        const handleScroll = () => {
-            if (
-                window.innerHeight + document.documentElement.scrollTop ===
-                document.documentElement.offsetHeight
-            ) {
-                setLimitor((prev) => prev + 20);
-            }
-        };
-
-        window.addEventListener("scroll", handleScroll);
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
-    }, []);
-
     return (
-        <main className="DamWaterQuality px-8 py-4 lg:w-3/4 mx-auto ">
-            <div className="container mx-auto flex flex-col justify-center items-center">
-                <div className="items-center mb-2">
-                    <h1 className="text-3xl font-bold text-center text-gray-800">
-                        Baraj Listesi
-                    </h1>
-                </div>
-            </div>
-            <div className="relative mx-auto max-w-md my-2">
-                <input
-                    type="text"
-                    placeholder="Ara"
-                    onChange={changeInput}
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                />
-                <span className="absolute inset-y-0 right-0 flex items-center pr-3">
-                    <IoIosSearch className="text-gray-500" />
-                </span>
-            </div>
-            <div className="card-container my-4 p-4 bg-white rounded-lg shadow-md">
-                {loading && <p>Loading...</p>}
-                {data && (
-                    <div className="gap-4 grid grid-cols-2">
-                        {data
-                            .filter((item) =>
-                                item.BarajAdi.toLowerCase().includes(
-                                    searchTerm.toLowerCase()
-                                )
-                            )
-                            .slice(0, limitor)
-                            .map((item, index) => (
+        <main className="p-8 w-[90%] sm:w-[80%] md:w-[70%] lg:w-[60%] mx-auto gap-4 flex flex-col">
+            <section className="flex flex-col justify-center items-center gap-4">
+                <h1 className="text-3xl font-bold text-center">
+                    Baraj Su Kalite Raporları
+                </h1>
+                <article className="flex items-center px-3 bg-white py-2 rounded-3xl border border-gray-300 text-2xl w-full md:w-[80%] lg:w-[50%]">
+                    <IoIosSearch className="mr-2" />
+                    <input
+                        type="text"
+                        placeholder="Mahalle veya ilçe ara"
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        className="text-lg bg-transparent border-none rounded w-full focus:outline-none focus:shadow-outline"
+                    />
+                </article>
+            </section>
+            {data.map((item, index) => (
+                <section
+                    key={index}
+                    className="p-4 border bg-white border-gray-300 rounded-2xl cursor-pointer hover:border-gray-400"
+                    onClick={() => setSelectedItem(item)}>
+                    <article className="my-4 flex flex-col md:flex-row items-center gap-4">
+                        <h2 className="text-xl font-semibold my-2">
+                            {item.BarajAdi}
+                        </h2>
+                        <p className="border w-fit p-2 rounded-md text-center">
+                            {formatDateTime(item.Tarih)}
+                        </p>
+                    </article>
+                </section>
+            ))}
+            <Modal show={selectedItem} onClose={() => setSelectedItem(null)}>
+                {selectedItem && (
+                    <div className="p-6">
+                        <div className="mb-6">
+                            <h2 className="text-2xl font-bold text-gray-800">
+                                {selectedItem.BarajAdi}
+                            </h2>
+                            <p className="text-gray-600 mt-1">
+                                <span className="font-medium">Tarih:</span>{" "}
+                                {formatDateTime(selectedItem.Tarih)}
+                            </p>
+                        </div>
+                        <div className="space-y-6">
+                            {selectedItem.Analizler.map((analiz, index) => (
                                 <div
                                     key={index}
-                                    className="card my-4 px-16 py-8 border hover:border-gray-400 transition duration-200 ease-in-out transform hover:scale-105 hover:shadow-md rounded-2xl"
-                                    onClick={() => handleItemClick(item)}>
-                                    <div>
-                                        <p className="text-gray-700 font-bold">
-                                            BarajAdi
-                                        </p>
-                                        <p className="text-gray-500">
-                                            {item.BarajAdi}
-                                        </p>
-                                    </div>
-                                    <div>
-                                        <p className="text-gray-700 font-bold">
-                                            Tarih
-                                        </p>
-                                        <p className="text-gray-500">
-                                            {formatDateTime(item.Tarih)}
-                                        </p>
+                                    className="border-b border-gray-200 pb-6 last:border-0">
+                                    <h3 className="text-xl font-semibold text-blue-600 mb-4">
+                                        {analiz.AnalizTipAdi}
+                                    </h3>
+
+                                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                                        {analiz.AnalizElemanlari.map(
+                                            (eleman, idx) => (
+                                                <div
+                                                    key={idx}
+                                                    className="bg-gray-50 p-4 rounded-lg">
+                                                    <h4 className="font-medium text-gray-800 mb-3">
+                                                        {eleman.ParametreAdi}
+                                                    </h4>
+
+                                                    <div className="space-y-2 text-sm">
+                                                        <div className="flex justify-between">
+                                                            <span className="text-gray-500">
+                                                                Standart:
+                                                            </span>
+                                                            <span className="font-medium">
+                                                                {
+                                                                    eleman.Standart
+                                                                }
+                                                            </span>
+                                                        </div>
+                                                        <div className="flex justify-between">
+                                                            <span className="text-gray-500">
+                                                                Birim:
+                                                            </span>
+                                                            <span className="font-medium">
+                                                                {eleman.Birim}
+                                                            </span>
+                                                        </div>
+                                                        <div className="flex justify-between">
+                                                            <span className="text-gray-500">
+                                                                İşlenmiş Su:
+                                                            </span>
+                                                            <span className="font-medium">
+                                                                {
+                                                                    eleman.IslenmisSu
+                                                                }
+                                                            </span>
+                                                        </div>
+                                                        <div className="flex justify-between">
+                                                            <span className="text-gray-500">
+                                                                İşlenmemiş Su:
+                                                            </span>
+                                                            <span className="font-medium">
+                                                                {
+                                                                    eleman.IslenmemisSu
+                                                                }
+                                                            </span>
+                                                        </div>
+                                                        {eleman.Regulasyon && (
+                                                            <div className="flex justify-between">
+                                                                <span className="text-gray-500">
+                                                                    Regülasyon:
+                                                                </span>
+                                                                <span className="font-medium">
+                                                                    {
+                                                                        eleman.Regulasyon
+                                                                    }
+                                                                </span>
+                                                            </div>
+                                                        )}
+                                                    </div>
+                                                </div>
+                                            )
+                                        )}
                                     </div>
                                 </div>
                             ))}
-                        {data.filter((item) =>
-                            item.BarajAdi.toLowerCase().includes(
-                                searchTerm.toLowerCase()
-                            )
-                        ).length === 0 && (
-                            <p>
-                                No data found for{" "}
-                                <span className="italic font-bold">
-                                    {searchTerm}
-                                </span>
-                            </p>
-                        )}
-                    </div>
-                )}
-            </div>
-            <Modal show={modalIsOpen} onClose={closeModal}>
-                {selectedItem && (
-                    <div className="modal-content ">
-                        <h2 className="text-2xl font-bold mb-4">
-                            {selectedItem.BarajAdi}
-                        </h2>
-                        <p className="mb-4">
-                            Tarih: {formatDateTime(selectedItem.Tarih)}
-                        </p>
-                        {selectedItem.Analizler.map((analiz, index) => (
-                            <div key={index} className="mb-4">
-                                <p className="text-lg font-semibold">
-                                    Analiz Tipi: {analiz.AnalizTipAdi}
-                                </p>
-                                {analiz.AnalizElemanlari.map(
-                                    (eleman, index) => (
-                                        <div
-                                            key={index}
-                                            className="p-2 border-b border-gray-200">
-                                            <p className="text-gray-700">
-                                                <span className="font-semibold">
-                                                    Parametre Adı:{" "}
-                                                </span>
-                                                {eleman.ParametreAdi}
-                                            </p>
-                                            <p className="text-gray-700">
-                                                <span className="font-semibold">
-                                                    Standart:{" "}
-                                                </span>
-                                                {eleman.Standart}
-                                            </p>
-                                            <p className="text-gray-700">
-                                                <span className="font-semibold">
-                                                    Birim:{" "}
-                                                </span>
-                                                {eleman.Birim}
-                                            </p>
-                                            <p className="text-gray-700">
-                                                <span className="font-semibold">
-                                                    İşlenmiş Su:{" "}
-                                                </span>
-                                                {eleman.IslenmisSu}
-                                            </p>
-                                            <p className="text-gray-700">
-                                                <span className="font-semibold">
-                                                    İşlenmemiş Su:{" "}
-                                                </span>
-                                                {eleman.IslenmemisSu}
-                                            </p>
-                                            {eleman.Regulasyon && (
-                                                <p className="text-gray-700">
-                                                    <span className="font-semibold">
-                                                        Regulasyon:{" "}
-                                                    </span>
-                                                    {eleman.Regulasyon}
-                                                </p>
-                                            )}
-                                        </div>
-                                    )
-                                )}
-                            </div>
-                        ))}
+                        </div>
                     </div>
                 )}
             </Modal>
